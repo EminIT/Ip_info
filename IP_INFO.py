717f97616a07507e1b59867833abe9ad15fcfddf
Xastaev <104794270+EminIT@users.noreply.github.com>
2022-07-11 23:54:55 +0300

    Update IP_INFO.py

--- IP_INFO.py
+++ IP_INFO.py
@@ -1,4 +1,4 @@
-import requests
+import requests         # import librares
 from pyfiglet import Figlet
 import folium
 from colorama import Fore
@@ -8,8 +8,7 @@ init()
 
 def get_info_by_ip(ip='127.0.0.1'):
     try:
-        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
-        # print(response)
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()      # information about ip adres
         
         data = {
             '[IP]': response.get('query'),
@@ -21,22 +20,22 @@ def get_info_by_ip(ip='127.0.0.1'):
             '[ZIP]': response.get('zip'),
             '[Lat]': response.get('lat'),
             '[Lon]': response.get('lon'),
-        }
+        }       # need's information
         
         for k, v in data.items():
             print(Fore.GREEN + f'{k} : {v}')
         
         area = folium.Map(location=[response.get('lat'), response.get('lon')])
-        area.save(f'{response.get("query")}_{response.get("city")}.html')
+        area.save(f'{response.get("query")}_{response.get("city")}.html')   #        create html file 
         
     except requests.exceptions.ConnectionError:
-        print(Fore.GREEN + '[!] Please check your connection!')
+        print(Fore.GREEN + '[!] Please check your connection!')     # except erorr
         
         
 def main():
     preview_text = Figlet(font='slant')
     print(Fore.GREEN + preview_text.renderText('IP INFO'))
-    ip = input(Fore.GREEN + 'Please enter a target IP: ')
+    ip = input(Fore.GREEN + 'Please enter a target IP: ')       # ip quetion
     
     get_info_by_ip(ip=ip)
     
